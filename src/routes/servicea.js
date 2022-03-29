5c58c32c8ef1743947af84f581ca36c8d7b713ad
bk <jase.bkthong@gmail.com>
2022-03-30 00:51:21 +0800

    Removed ip address code and only left with showing hostname
    Due to ip showwing up as 169.254.x.x when on ecs fargate

--- src/routes/servicea.js
+++ src/routes/servicea.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const ip = require('ip');
-const ipAddress = ip.address() ;
+
 var os = require("os");
 var hostname = os.hostname();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('<div style="font-family: Verdana ; font-size: 28">Hello from servicea: ' + ipAddress + ', ' + hostname + '</div>');
+  res.send('<div style="font-family: Verdana ; font-size: 28">Hello from servicea: ' +  hostname + '</div>');
 });
 
 module.exports = router;
