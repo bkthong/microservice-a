da0214565f132a13763f9abcf754dc6650c43b5a
bk <jase.bkthong@gmail.com>
2022-03-30 00:28:39 +0800

    Updated code to show hostname as well

--- src/routes/servicea.js
+++ src/routes/servicea.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const ip = require('ip');
-const ipAddress = ip.address() ;
-
+const ipAddress = ip..address() ;
+var os = require("os");
+var hostname = os.hostname();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('<div style="font-family: Verdana ; font-size: 28">Hello from servicea: ' + ipAddress + '</div>');
+  res.send('<div style="font-family: Verdana ; font-size: 28">Hello from servicea: ' + ipAddress + ', ' + hostname + '</div>');
 });
 
 module.exports = router;
